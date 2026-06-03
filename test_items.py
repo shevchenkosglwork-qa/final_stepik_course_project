6c4067b0604a41beb015da07c3b23871533f495d
grigorii <gregshevch@gmai.com>
2026-06-03 21:39:12 +0500

    First commit: 3.6 step 10

--- test_items.py
+++ test_items.py
@@ -1,23 +1,24 @@
-from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 from selenium.webdriver.common.by import By
-
-from conftest import browser
 
 link = ' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_open_shop(browser):
+    #Переходим по ссылке
     browser.get(link)
+    #Находим элемент и ждем, когда появится кнопка добавить в корзину
     put_in_the_basket = (WebDriverWait(browser, 10)
                          .until(EC.presence_of_element_located(
         (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket"))))
+
+    #Кликаем по кнопке
     put_in_the_basket.click()
+
+    #Находим текст, что товар добавлен
     result = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located(
-            browser.find_element(By.CLASS_NAME, "alertinner")))
-    #result = browser.find_element(By.ID, "write_review")
+            (By.CLASS_NAME, "alertinner")))
 
     assert result.is_displayed(), 'Товар не добавился'
-    print("\n Товар успешно добавился и отображается на экране!")
+    print("\n --- Товар успешно добавился и отображается на экране! --- \n")
