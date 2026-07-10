71572272c37ab970fe9c5102e5e4969d5e566402
grigorii <gregshevch@gmai.com>
2026-07-10 21:31:32 +0500

    new params timeout and new assert

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,18 @@
+from selenium.common import NoSuchElementException
+
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, 'login_link_invalid'), 'login link is not present'
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
